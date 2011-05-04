b26cd9260e9b9679a7621a32f3c721cac88b1bfc
Moses <nnythm@gmail.com>
2011-05-04 13:52:59 -0400

    added list decl stuff

--- V.1/flow/ast/ListDec.java
+++ V.1/flow/ast/ListDec.java
@@ -2,12 +2,13 @@ package flow.ast;
 
 public class ListDec extends StatementNode {
 
-	public ListDec( Type type, ID id, AttrList alist)
+	public ListDec(Type type, ID id, AttrList alist)
 	{
 		this.id = id;
 		this.aList = aList;
 		if (type.type == "int") this.type = "Integer";
 		else if (type.type == "double") this.type = "Double";
+		else if (type.type.substring(0,4) == "list") this.type = "ArrayList";
 		else this.type = type.toString();
 	}
 
@@ -16,11 +17,14 @@ public class ListDec extends StatementNode {
 	private AttrList aList;
 
 	public String toString() {
-		//if (aList != null) {
-			return "ArrayList<" + type + "> " + id + " = new ArrayList<" + type + ">((List<" + type + ">) Arrays.asList("+aList+"))";
-		//}
-		//else {
-		//	return "ArrayList<" + type + "> " + id + " = new ArrayList<" + type + ">()";
-		//}
+	    if (aList != null) {
+		String str;
+		str = type + "[] tmp = {" + alist.toString() + "};\n";
+		str +=  "ArrayList<" + type + "> " + id + " = new ArrayList<" + type + ">((List<" + type + ">) Arrays.asList(tmp))";
+		return str;
+	    }
+	    else {
+		return "ArrayList<" + type + "> " + id + " = new ArrayList<" + type + ">()";
+	    }
 	}
 }
