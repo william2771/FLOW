2f2566b601bd77d07d1af1406ffa64c18eec4448
Mark Liu <ml2877@colubmia.edu>
2011-05-07 00:52:27 -0400

    WTFgit add .git add .

--- V.1/Arc.java
+++
@@ -1,9 +0,0 @@
-public class Arc extends flow.structure.SuperArc {
-  public Arc(Node source, Node dest, int transition) {
-    super(source, dest);
-    this.transition = transition;
-  }
-  private int transition;
-  public int gettransition()
-  { return transition; }
-}
